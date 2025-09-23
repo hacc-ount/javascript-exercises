a73b6b8a9abd0c2bffa9a49041c8e0e9fa8c28e0
hacc-ount <214715279+hacc-ount@users.noreply.github.com>
2025-09-23 04:34:19 +0100

    Add variables to joinStrings to have the tests under step 3 in joinStrings.spec.js pass

--- 05_joinStrings/joinStrings.js
+++ 05_joinStrings/joinStrings.js
@@ -14,7 +14,10 @@ const lastName = `Stevenson`;
 const thisYear = 1965;
 const birthYear = 1947;
 
-let greeting = `Hello! My name is ` + firstName + ` ` + lastName + ` and I am ` + (thisYear - birthYear) + ` years old.`;
+const fullName = firstName + ` ` + lastName;
+const age = thisYear - birthYear;
+
+let greeting = `Hello! My name is ` + fullName + ` and I am ` + age + ` years old.`;
 
 
 
