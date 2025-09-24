152986a2cdca042405ede0f30bb65fd010877d59
hacc-ount <214715279+hacc-ount@users.noreply.github.com>
2025-09-24 02:16:21 +0100

    Edit reverseString.js to return the reverse value of an inputted string

--- 07_reverseString/reverseString.js
+++ 07_reverseString/reverseString.js
@@ -1,4 +1,7 @@
-const reverseString = function() {
+const reverseString = function(string) {
+
+    // Make an array from string input, reverse it using reverse array method, then join using the join array method.
+    return Array.from(string).toReversed().join(``);
 
 };
 
