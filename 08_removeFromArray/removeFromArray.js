db7f5077950e231ef6f5b63a7da492e4aa9b5c01
hacc-ount <214715279+hacc-ount@users.noreply.github.com>
2025-09-24 05:50:52 +0100

    Commit this solution, but couldn't figure out how to do it. I learnt that rest parameters exist and ttried to use a for(const a of array) loop to loop over the rest parameters and then another for(const b of array) loop to loop inside of that, but it wasn't giving me the right result. In the end I had to look at the solution to learn that you can just run a .includes() method treating it as an array.

--- 08_removeFromArray/removeFromArray.js
+++ 08_removeFromArray/removeFromArray.js
@@ -1,4 +1,17 @@
-const removeFromArray = function() {
+const removeFromArray = function(arr, ...values) {
+
+    let newArr = [];
+
+    arr.forEach((item) => {
+        if (!values.includes(item)) {
+            newArr.push(item);
+        } else {
+
+        }
+    })
+
+    return newArr;
+
 };
 
 // Do not edit below this line
