d3b24ff97480795229e954293b11227abe6c7998
hacc-ount <214715279+hacc-ount@users.noreply.github.com>
2025-09-25 01:37:56 +0100

    Add something in to return an error for certain clauses. Used Math.sign() to check if number is negative or positive, this also works for things that can't be convered to a number such as an array of two numbers. This doesn't stop a string with number inside being parsed or a number with a decimal place. Want to find a way to have a very simple check work for all the tests.

--- 09_sumAll/sumAll.js
+++ 09_sumAll/sumAll.js
@@ -1,12 +1,26 @@
 const sumAll = function(a, b) {
-    let descendingNum = b - 1;
-    let calculation = b;
-    while (descendingNum >= a) {
-        calculation += descendingNum;
-        descendingNum --;
+
+    if (Math.sign(a) !== 1 || Math.sign(b) !== 1) {
+        return `ERROR`;
+    } else if (a < b) {
+        let descendingNum = b - 1;
+        let calculation = b;
+        while (descendingNum >= a) {
+            calculation += descendingNum;
+            descendingNum --;
+        }
+        return calculation;
+    } else {
+        let descendingNum = a - 1;
+        let calculation = a;
+        while (descendingNum >= b) {
+            calculation += descendingNum;
+            descendingNum --;
+        }
+        return calculation;
     }
-    return calculation;
-};
+
+}
 
 // Do not edit below this line
 module.exports = sumAll;
