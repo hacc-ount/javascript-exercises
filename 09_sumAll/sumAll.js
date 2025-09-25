a54ef2bf8bd846c1974084d755c9ce441f1383d6
hacc-ount <214715279+hacc-ount@users.noreply.github.com>
2025-09-25 05:56:32 +0100

    Add code to sumAll.js using the solution file learning. so that I don't have to write two seperate if/ifelse statements that contain seperate loops, I have just reviewed the solution file to learn about this scenario of needing to test for the larger number. It is a more clean solution where only one if statement needs to be written and within that it is holding the statement for the numbers being the wrong way around. Here all that happens is that if a is larger than b, then the varibales take on each others values and this can only be done by introducing anther variable that can temporarily store the larger varible so that we can assign that later again. After that a loop is placed underneath the code doing the exact same as I did previously but with a for loop. I like this structure because it means code isn't repeated (two loops previously) and it just does a very simple swap before the main loop is executed only if the if condition statement is met.

--- 09_sumAll/sumAll.js
+++ 09_sumAll/sumAll.js
@@ -25,6 +25,20 @@ const sumAll = function(a, b) {
 if (!Number.isInteger(a) || !Number.isInteger(b)) return `ERROR`;
 if (a < 0 || b < 0) return `ERROR`;
 
+if (a > b) {
+    let temp = b;
+    b = a;
+    a = temp;
+} //swaps numbers around to have biggest always in `b's` position.
+
+let calculation = b;
+
+for (let descender = b - 1; descender >= a; descender--) {
+    calculation += descender;
+}
+
+return calculation;
+
 }
 
 // Do not edit below this line
