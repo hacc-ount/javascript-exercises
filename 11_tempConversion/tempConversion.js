b398b8bc6a83873a2e10860324bc3c7c96eb8757
hacc-ount <214715279+hacc-ount@users.noreply.github.com>
2025-10-01 05:06:48 +0100

    Add conversions for each function. return those values to one decimal place.

--- 11_tempConversion/tempConversion.js
+++ 11_tempConversion/tempConversion.js
@@ -1,7 +1,11 @@
-const convertToCelsius = function() {
+const convertToCelsius = function(number) {
+  let converted = (number - 32) * 5/9;
+  return Number(converted.toFixed(1));
 };
 
-const convertToFahrenheit = function() {
+const convertToFahrenheit = function(number) {
+  let converted = (number * 9/5) + 32;
+  return Number(converted.toFixed(1));
 };
 
 // Do not edit below this line
